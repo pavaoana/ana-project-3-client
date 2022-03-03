407e5ce56805485183d49136a92bdb58aed2c976
pavaoana <anapavao2@gmail.com>
2022-03-03 13:21:45 +0000

    create necessary components

---
+++ src/components/IsAnon.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function IsAnon() {
+  return <div>IsAnon</div>;
+}
---
+++ src/components/TopicCreate.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function TopicCreate() {
+  return <div>TopicCreate</div>;
+}
