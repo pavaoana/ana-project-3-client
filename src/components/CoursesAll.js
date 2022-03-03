df3beb0e298ea78f131eb95c996e0e1b66a09317
pavaoana <anapavao2@gmail.com>
2022-03-03 16:55:21 +0000

    done: basic show all courses page working

--- src/components/CoursesAll.js
+++ src/components/CoursesAll.js
@@ -1,5 +1,36 @@
 import React from "react";
 
-export default function CoursesAll() {
-  return <div>CoursesAll</div>;
+export default function CoursesAll(props) {
+  return (
+    <div className="AllCourses">
+      {props.courses.map((course) => {
+        return (
+          <div className="course-card" key={course._id}>
+            <div className="course-img">
+              <img src={course.image} alt={course.courseName} />
+            </div>
+            <div className="course-details">
+              <h3>{course.courseName}</h3>
+              <p>{course.description}</p>
+              <p>{course.topics}</p>
+              <p>Location: {course.location}</p>
+              <p>Duration: {course.duration}</p>
+              <p>Schedule: {course.schedule}</p>
+              <p>Career Services Offered? {course.careerServices}</p>
+              <p>A Job at the End? {course.jobGuaranteed}</p>
+              <p>Pre-Requisites? {course.preRequisites}</p>
+              <p>Price: {course.cost}</p>
+              <p>
+                Click{" "}
+                <a href={course.link} target="_blank">
+                  here
+                </a>{" "}
+                if you want to know more.
+              </p>
+            </div>
+          </div>
+        );
+      })}
+    </div>
+  );
 }
